b6fd1efce47ea1d8c4f86f7c80613329c5487632
Diego Cantelli <diegocantelli19@gmail.com>
2021-05-11 19:40:50 -0300

    Utilizando a função Aggregate

--- LinqDemo/LinqDemo/Program.cs
+++ LinqDemo/LinqDemo/Program.cs
@@ -8,6 +8,7 @@ namespace LinqDemo
         static void Main(string[] args)
         {
             int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            string[] countries = { "Brasil", "India", "EUA", "UK" };
 
             //Obtendo o menor valor de um array
             int result = numbers.Min();
@@ -15,7 +16,19 @@ namespace LinqDemo
             //Obtendo o menor dentro os valores pares
             result = numbers.Where(x => x % 2 == 0).Min();
 
-            Console.WriteLine(result);
+            //a -> 0
+            //b -> 1
+            //faz com que seja concatenado as posições do array até o último elemento
+            //O que foi concatenado, na próxima iteração, volta como sendo o valor do primeiro parâmetro.
+            //O Primeiro parâmetro funciona como uma variável acumuladora
+            //Resultado -> "Brasil, India, EUA, UK" 
+            var aggregate = countries.Aggregate((a, b) => a + ", " + b);
+
+            //É possível passar um parâmetro inicial nesta função, ela será usada como o valor inicial da iteração
+            //Resultado -> "Chile, Brasil, India, EUA, UK" 
+            var aggregateSeed = countries.Aggregate("Chile ", (a, b) => a + ", " + b);
+
+            Console.WriteLine(aggregateSeed);
             Console.Read();
         }
     }
